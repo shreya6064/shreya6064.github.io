376de79f180cf985953e4fa8b87722b2dc68b5a5
Shreya Punjabi <spunjabi7@gatech.edu>
2025-12-10 19:41:09 -0500

    added projects support

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/', // main site at https://username.github.io/
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+      },
+    },
+  },
 });
